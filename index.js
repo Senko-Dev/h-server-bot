e770c2c8c659ee40bf6c5a9d53d504ba4b94a036
Glitch (hello-express) <none>
2020-06-16 20:21:03 +0000

    ⛹🌥 Checkpoint ./index.js:8023934/1251

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Discord = require("discord.js");
 const Client = new Discord.Client();
 
 Client.on("ready", () => {
-  console.log('qwertyuiopasdfghjklzxcvbnm');
-  Client.user.setActivity("People Send Letters And Numbers!", {type:"WATCHING"})
-})
+  console.log("qwertyuiopasdfghjklzxcvbnm");
+  Client.user.setActivity("People Send Letters And Numbers!", {
+    type: "WATCHING"
+  });
+});
 
-Client.on("message", () => {
+Client.on("message", message => {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  if(message.content.includes(numbers)) {
-    message.delete();
+    if (message.content.includes(numbers)) {
+      message.delete();
+      message
   }
-})
+});
 Client.login(process.env.TOKEN);
