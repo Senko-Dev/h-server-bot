80552d4bf9402c108521129c294ff2dcb35f5164
Glitch (hello-express) <none>
2020-07-24 19:13:59 +0000

    📈👛 Checkpoint ./index.js:8023934/16233

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 
 process.on("unhandledRejection", error => {
   console.error(error);
@@ -17,14 +18,30 @@ client.on("ready", () => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.content !== "h" && message.channel.id !== ) {
+  if (message.content !== "h" && message.channel.id !== "736294939968929844") {
     message.delete({ timeout: 2000 }).then(() => {
-      message.reply(
-        `you G spy. You think you can deefat me in my owne realm? You think you can rebel aqaisnt my authority? You dare come into my house and spill G all my over my H. You thouqht you were safe behind that G of yours? I will take my H an destroy you. I did want war. It is illeqal to say anythinq other than \`h\` in this server.`
-      ).then(() => {
-        message.delete({ timeout: 6000 })
-      })
+      message
+        .reply(
+          `you G spy. You think you can deefat me in my owne realm? You think you can rebel aqaisnt my authority? You dare come into my house and spill G all my over my H. You thouqht you were safe behind that G of yours? I will take my H an destroy you. I did want war. It is illeqal to say anythinq other than \`h\` in this server.`
+        )
+        .then(() => {
+          message.delete({ timeout: 6000 });
+        });
     });
+  }
+  if(message.content !== 'hs' && message.channel.id === '736297919958876211') {
+        message.delete({ timeout: 2000 }).then(() => {
+      message
+        .reply(
+          `you can only say \`hs\` in this channel.`
+        )
+        .then(() => {
+          message.delete({ timeout: 6000 });
+        });
+    });
+  }
+  if(message.content === 'hs' && message.channel.id === '736297919958876211') {
+    message.reply('This is)
   }
 });
 client.login(process.env.TOKEN);
