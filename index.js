455f8a8c3aff654d89d02120f7bbbcea91be577d
Glitch (hello-express) <none>
2020-07-24 18:05:08 +0000

    🐿🚎 Checkpoint ./package.json:8023934/841 ./index.js:8023934/774

---
+++ index.js
@@ -0,0 +1,18 @@
+  const Discord = require('discord.js');
+  const client = new Discord.Client();
+
+  process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+  });
+
+  client.on('ready', client => {
+    client.user.setActivity('h');
+    console.log('h');
+  })
+
+  client.on('message', message => {
+    if(message.content !== "h") {
+      message.delete();
+    }
+  })
+  client.login(process.env.TOKEN);
