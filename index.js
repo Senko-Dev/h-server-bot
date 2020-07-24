da74b4b8a6d4affc88ab9ea7bd0b10e8d3bcfe02
Glitch (hello-express) <none>
2020-07-24 19:25:31 +0000

    🍜🌉 Checkpoint ./index.js:8023934/4466

--- index.js
+++ index.js
@@ -18,30 +18,32 @@ client.on("ready", () => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.content !== "h" && message.channel.id !== "736294939968929844") {
+  if (message.content !== "h" && message.channel.id !== "736297919958876211") {
     message.delete({ timeout: 2000 }).then(() => {
       message
         .reply(
-          `you G spy. You think you can deefat me in my owne realm? You think you can rebel aqaisnt my authority? You dare come into my house and spill G all my over my H. You thouqht you were safe behind that G of yours? I will take my H an destroy you. I did want war. It is illeqal to say anythinq other than \`h\` in this server.`
+          `you G spy. You think you can deefat me in my own realm? You think you can rebel aqaisnt my authority? You dare come into my house and spill G all my over my H. You thouqht you were safe behind that G of yours? I will take my H and destroy you. I did want war. It is illeqal to say anythinq other than \`h\` in this server.`
         )
         .then(() => {
           message.delete({ timeout: 6000 });
         });
     });
   }
-  if(message.content !== 'hs' && message.channel.id === '736297919958876211') {
-        message.delete({ timeout: 2000 }).then(() => {
-      message
-        .reply(
-          `you can only say \`hs\` in this channel.`
-        )
-        .then(() => {
+  if (message.channel.id === "736297919958876211") {
+    if (
+      message.content !== "hs"
+    ) {
+      message.delete({ timeout: 2000 }).then(() => {
+        message.reply(`you can only say \`hs\` in this channel.`).then(() => {
           message.delete({ timeout: 6000 });
         });
-    });
-  }
-  if(message.content === 'hs' && message.channel.id === '736297919958876211') {
-    message.reply('This is)
+      });  
+    }
+    if (
+      message.content === "hs"
+    ) {
+      message.reply("This isn't made yet.");
+    }
   }
 });
 client.login(process.env.TOKEN);
