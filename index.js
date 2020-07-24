15d1b5b06e56e74df0eeabb7f5f8e8b2605dc538
Glitch (hello-express) <none>
2020-07-24 18:50:21 +0000

    🚨🏐 Checkpoint ./index.js:8023934/131

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 process.on("unhandledRejection", error => {
-  console.error("Unhandled promise rejection:", error);
+  console.error(error);
 });
 
 client.on("guildMemberAdd", member => {
   member.setNickname("h");
-  member.role.add('736289092307910768');
-  
+  member.role.add('736289092307910768'); 
 });
 
 client.on("ready", () => {
@@ -17,7 +16,8 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-  if (message.content !== "h" && message.author.bot) {
+  if(message.author.bot) return;
+  if (message.content !== "h") {
     message.delete();
     message.reply(
       `,you G spy. You think you can deefat me in my owne realm? You think you can rebel aqaisnt my authority? You dare come into my house and spill G all my over my H. You thouqht you were safe behind that G of yours? I will take my H an destroy you. I did want war. It is illeqal to say anythinq other than \`h\` in this server.`
